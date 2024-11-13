12a814cb57d50252c370c654de1618f1de2fc1ad
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-11-13 17:16:43 +0100

    move main app

--- src/oc_lettings_site/settings.py
+++ src/oc_lettings_site/settings.py
@@ -28,6 +28,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'oc_lettings_site'
+    'lettings',
+    'profils',
 ]
 
 MIDDLEWARE = [
@@ -111,4 +114,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static",]
+STATICFILES_DIRS = [BASE_DIR / "oc_lettings_site/static",]
