0e6cf1a44c4ed2229dfb52e85b00ff52747af271
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-11-18 10:45:12 +0100

    add custom 404 and 500 pages

--- src/oc_lettings_site/urls.py
+++ src/oc_lettings_site/urls.py
@@ -3,9 +3,13 @@ from django.urls import path, include
 
 from . import views
 
+# error handling
+handler404 = "oc_lettings_site.views.custom_404"
+handler500 = "oc_lettings_site.views.custom_500"
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls')),
-    path('profiles/', include('profils.urls')),
+    path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
 ]
