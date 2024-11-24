c7f0d647b370da59b2a2a1bbae31e7b0488d0633
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-11-24 16:22:09 +0100

    change collectstatic verbosity

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-
-echo "Applying database migrations..."
+set -e  # on arrete le script en cas d'erreur
+echo "Apply database migrations..."
 python manage.py migrate --noinput
 
 # On lance collectstatic (regroupe les static dans un rep unique)
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
+echo "collect static files..."
+python manage.py collectstatic --noinput --verbosity 2
 
-echo "Starting Gunicorn..."
+# TODO param a mettre en variable d'env
+echo "Start Gunicorn..."
 exec gunicorn oc_lettings_site.wsgi:application --chdir /app/src --bind 0.0.0.0:8080
