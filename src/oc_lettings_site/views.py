1b25bb04cf93cc265076ebe5de5bdbc96b1aca12
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-12-06 15:52:58 +0100

    add logs in oc-lettings-site app

--- src/oc_lettings_site/views.py
+++ src/oc_lettings_site/views.py
@@ -2,10 +2,12 @@
 oc_lettings_site/views.py
 Contains the Django views for rendering the home page and custom error pages.
 """
+import logging
+
 from django.shortcuts import render
 from django.http import JsonResponse
 
-
+logger = logging.getLogger(__name__)
 # Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 # Quisque molestie quam lobortis leo consectetur ullamcorper non id est.
 # Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros,
@@ -25,6 +27,7 @@ def index(request):
     Returns:
         HttpResponse: The rendered home page (index.html)
     """
+    logger.info('Home page "index.html" accessed.')
     return render(request, 'oc_lettings_site/index.html')
 
 
@@ -38,6 +41,7 @@ def health(request):
     Returns:
         JsonResponse: Json response with status code 200
     """
+    logger.info("Health check endpoint accessed.")
     # TODO : add db connect try
     return JsonResponse({"status": "ok"}, status=200)
 
@@ -53,6 +57,7 @@ def custom_404(request, exception):
     Returns:
         HttpResponse: The rendered 404 error page with a 404 status code
     """
+    logger.warning(f"404 error. Path: {request.path}. Exception: {exception}")
     return render(request, "404.html", status=404)
 
 
@@ -69,4 +74,5 @@ def custom_500(request):
     print("=== Entering custom_500 handler ===")
     print("Request path:", request.path)
     print("Request method:", request.method)
+    logger.critical(f"500 error. Path: {request.path}, Method: {request.method}")
     return render(request, "500.html", status=500)
